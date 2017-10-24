67d8d83175d0863425d9423778a65fe58680bbeb
Rishav1996 <rishavsaigal@gmail.com>
2017-10-24 14:18:16 +0530

    Test

--- app/src/main/java/com/tenpitech/apmd/apmdsdk/service/AllBackgroundService.java
+++ app/src/main/java/com/tenpitech/apmd/apmdsdk/service/AllBackgroundService.java
@@ -1,11 +1,20 @@
 package com.tenpitech.apmd.apmdsdk.service;
 
 import android.app.Service;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 
-public class AllBackgroundService extends Service {
+import com.tenpitech.apmd.apmdsdk.BleWrapperUiCallbacks;
+import com.tenpitech.apmd.apmdsdk.bean.SensorObject;
+
+import java.util.List;
+
+public class AllBackgroundService extends Service implements BleWrapperUiCallbacks {
     public AllBackgroundService() {
     }
 
@@ -18,5 +27,85 @@ public class AllBackgroundService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void uiDeviceFound(BluetoothDevice device, int rssi, byte[] record) {
+
+    }
+
+    @Override
+    public void uiDeviceConnected(BluetoothGatt gatt, BluetoothDevice device) {
+
+    }
+
+    @Override
+    public void uiDeviceDisconnected(BluetoothGatt gatt, BluetoothDevice device) {
+
+    }
+
+    @Override
+    public void uiScanFinished() {
+
+    }
+
+    @Override
+    public void uiScanStarted() {
+
+    }
+
+    @Override
+    public void uiAvailableServices(BluetoothGatt gatt, BluetoothDevice device, List<BluetoothGattService> services) {
+
+    }
+
+    @Override
+    public void uiCharacteristicForService(BluetoothGatt gatt, BluetoothDevice device, BluetoothGattService service, List<BluetoothGattCharacteristic> chars) {
+
+    }
+
+    @Override
+    public void uiCharacteristicsDetails(BluetoothGatt gatt, BluetoothDevice device, BluetoothGattService service, BluetoothGattCharacteristic characteristic) {
+
+    }
+
+    @Override
+    public void uiNewValueForCharacteristic(BluetoothGatt gatt, BluetoothDevice device, BluetoothGattService service, BluetoothGattCharacteristic ch, String strValue, int intValue, byte[] rawValue, String timestamp) {
+
+    }
+
+    @Override
+    public void uiNewValueForCharacteristicWithSensorObject(BluetoothGatt gatt, BluetoothDevice device, BluetoothGattService service, BluetoothGattCharacteristic ch, List<SensorObject> sensorObjects, int intValue, byte[] rawValue, String timestamp) {
+
+    }
+
+    @Override
+    public void uiGotNotification(BluetoothGatt gatt, BluetoothDevice device, BluetoothGattService service, BluetoothGattCharacteristic characteristic) {
+
+    }
+
+    @Override
+    public void uiSuccessfulWrite(BluetoothGatt gatt, BluetoothDevice device, BluetoothGattService service, BluetoothGattCharacteristic ch, String description) {
+
+    }
+
+    @Override
+    public void uiFailedWrite(BluetoothGatt gatt, BluetoothDevice device, BluetoothGattService service, BluetoothGattCharacteristic ch, String description) {
+
+    }
+
+    @Override
+    public void uiNewRssiAvailable(BluetoothGatt gatt, BluetoothDevice device, int rssi) {
+
+    }
+
+    @Override
+    public void uiServicesDiscovered(BluetoothGatt gatt, BluetoothGattService service) {
+
+    }
+
+    @Override
+    public void uiIsNotificationEnabled(boolean enable) {
+
     }
 }
